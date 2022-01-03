3a57ec8d5cef0e5a7e4963c6f0845a77b16d7454
apolkingg8 <apolkingg8@gmail.com>
2022-01-03 18:05:01 +0800

    :fire: [optimizer] remove imagemin

--- src/optimizer/JpegOptimizer.ts
+++ src/optimizer/JpegOptimizer.ts
@@ -1,11 +1,6 @@
 import IOptimizer from '../Interface/IOptimizer'
 import { EnumFileFormat } from '../Enum/EnumFileFormat'
 import EnumJpegOptimizeAlgo from '../Enum/EnumJpegOpitmizeAlgo'
-import imagemin from 'imagemin'
-import imageminJpegoptim from 'imagemin-jpegoptim'
-import imageminJpegRecompress from 'imagemin-jpeg-recompress'
-import imageminJpegTran from 'imagemin-jpegtran'
-import imageminMozJpeg from 'imagemin-mozjpeg'
 
 export default class JpegOptimizer implements IOptimizer {
   acceptFormats: EnumFileFormat[] = [EnumFileFormat.jpg, EnumFileFormat.jpeg]
@@ -17,37 +12,12 @@ export default class JpegOptimizer implements IOptimizer {
   ): Promise<Buffer> {
     switch (algo) {
       case EnumJpegOptimizeAlgo.jpegOptim:
-        return await imagemin.buffer(buffer, {
-          plugins: [
-            imageminJpegoptim({
-              max: quality,
-            }),
-          ],
-        })
+
       case EnumJpegOptimizeAlgo.jpegRecompress:
-        return await imagemin.buffer(buffer, {
-          plugins: [
-            imageminJpegRecompress({
-              quality: quality,
-            }),
-          ],
-        })
-      case EnumJpegOptimizeAlgo.jpegTran: {
-        return await imagemin.buffer(buffer, {
-          plugins: [
-            // no quality option for this algo
-            imageminJpegTran(),
-          ],
-        })
-      }
+
+      case EnumJpegOptimizeAlgo.jpegTran:
+
       case EnumJpegOptimizeAlgo.mozJpeg:
-        return await imagemin.buffer(buffer, {
-          plugins: [
-            imageminMozJpeg({
-              quality: quality,
-            }),
-          ],
-        })
     }
 
     throw `Algo ${algo} is not supported`
