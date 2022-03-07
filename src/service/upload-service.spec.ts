44451e32a7a0909774692c9d316c5f423bda3846
apolkingg8 <apolkingg8@gmail.com>
2022-03-07 16:34:12 +0800

    :white_check_mark: [upload] Improve upload test #8

--- src/service/upload-service.spec.ts
+++ src/service/upload-service.spec.ts
@@ -1,6 +1,8 @@
 import uploadService, { UploadService } from './upload-service'
 import { S3Client } from '@aws-sdk/client-s3'
 import { mockClient, mockLibStorageUpload } from 'aws-sdk-client-mock'
+import { Progress } from "@aws-sdk/lib-storage/dist-types/types";
+import _ from "lodash";
 
 describe('uploadService', () => {
   it('constructor()', async () => {
@@ -8,11 +10,25 @@ describe('uploadService', () => {
   })
 
   it('toS3()', async () => {
+    const onProgressMock = jest.fn()
+    const originOnProgressFn = uploadService.onS3UploadProgress
+    uploadService.onS3UploadProgress = onProgressMock
+
     const s3ClientMock = mockClient(S3Client)
     mockLibStorageUpload(s3ClientMock)
-    uploadService.s3Client = s3ClientMock as any
 
-    const buffer = Buffer.from([])
-    await uploadService.toS3(buffer, 'us-west', 'bucket')
+    const buffer = Buffer.from('x'.repeat(12 * 1024 * 1024)) // 12MB
+    const bucket = 'the-bucket'
+    await uploadService.toS3(buffer, 'us-west', bucket)
+
+    let progressArr: Progress[] = _.map(onProgressMock.mock.calls, (r)=> (r[0]))
+
+    expect(progressArr.length).toEqual(3)
+    expect(progressArr[0].Bucket).toEqual(bucket)
+    expect(progressArr[1].Bucket).toEqual(bucket)
+    expect(progressArr[2].Bucket).toEqual(bucket)
+    expect(progressArr[0].loaded).toBeLessThan(progressArr[1].loaded)
+    expect(progressArr[1].loaded).toBeLessThan(progressArr[2].loaded)
+    expect(progressArr[2].loaded).toEqual(buffer.length)
   })
 })
